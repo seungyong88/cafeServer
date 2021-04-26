a7c2ee243781c0e08db025d37e0d75cccba951e9
sygo <seungyong@saltlux.com>
2021-04-26 17:38:40 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Server-side: app.js
+const express    = require('express');
+const app = express();
+const port = 3003;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+// parse application/json
+
+app.get('/', (req, res) => res.send('Hello Worlds!'));
+
+app.post('/signin', function(req, res) {
+  const { username, password } = req.body;
+
+  console.log(username, password)
+  // 클라이언트로부터 전송된 페이로드를 그대로 response한다.
+  res.send({username, password});
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
